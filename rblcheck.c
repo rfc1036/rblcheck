1c98ef3251ed9c5e79302bb4696b41da2af01c51
Marco d'Itri <md@linux.it>
2019-08-30 01:47:19 +0200

    Remove the CVS tag

--- rblcheck.c
+++ rblcheck.c
@@ -16,9 +16,6 @@
 ** You should have received a copy of the GNU General Public License
 ** along with this program; if not, write to the Free Software
 ** Foundation, Inc., 675 Mass Ave, Cambridge, MA 02139, USA.
-**
-** $Id: rblcheck.c,v 1.14 2001/11/08 22:05:27 logic Exp $
-**
 */
 
 #include "config.h"
